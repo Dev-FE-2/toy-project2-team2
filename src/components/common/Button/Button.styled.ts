729b6defa5bc84864e858b039166f037882abc0a
mgyang53 <mg960503@gmail.com>
2024-11-25 15:01:10 +0900

    폴더 구조 세팅 완료

---
+++ src/components/common/Button/Button.styled.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+import { ButtonType } from "../../../types/compontents/button";
+
+// 버튼의 기본 스타일
+export const StyledButton = styled.button<{ buttonType: ButtonType }>``;
