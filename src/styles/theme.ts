22381138adf05b6943dc910a4f1cfcdb48c349a1
mgyang53 <mg960503@gmail.com>
2024-11-27 15:18:38 +0900

    feat : 테마 설정 파일 추가-#7

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,78 @@
+import { DefaultTheme } from "styled-components";
+
 // Styled-Components 테마 정의
-// App 컴포넌트를 감싸는 ThemeProvider 적용 필요
-export const theme = {
+export const theme: DefaultTheme = {
 	colors: {
-		primary: "#007bff",
-		secondary: "#6c757d",
-		danger: "#dc3545",
+		primary: "#029688",
+
+		secondaryDark: "#212B36",
+		secondary: "#637381",
+		secondaryLight: "#DFE4EA",
+
+		danger: "#E10E0E",
+		warning: "#FBBF24",
+		orange: "#F27430",
+		success: "#22AD5C",
+
+		grayDark: "#6B7280",
+		gray: "#9CA3AF",
+		grayLight: "#E5E7EB",
+		grayWhite: "#F3F4F6",
+
+		white: "#FFFFFF",
+
+		dim: "rgba(0,0,0,0.7)",
+	},
+
+	fontSize: {
+		xs: "12px",
+		sm: "14px",
+		md: "16px",
+		lg: "20px",
+		xl: "24px",
+	},
+
+	fontWeight: {
+		light: 300,
+		regular: 400,
+		medium: 500,
+		bold: 700,
+	},
+
+	breakpoints: {
+		sm: "320px",
+		md: "768px",
+		lg: "992px",
+		xl: "1280px",
+		xxl: "1440px",
 	},
 };
+
+/**
+ * @param section 테마 속성의 분류 (e.g. colors, breakpoints...)
+ * @param key 분류 안에 key 값 (e.g. color -> primary)
+ * @returns 테마의 값을 반환하는 함수
+ *
+ * 아래와 같이 사용 가능
+ * const primaryColor = getThemeValue("colors", "primary");
+ */
+export const getThemeValue =
+	<T extends keyof DefaultTheme>(section: T, key: keyof DefaultTheme[T]) =>
+	({ theme }: { theme: DefaultTheme }) =>
+		theme[section][key];
+
+// theme의 colors 부분 가져오는 함수
+export const getColor = (key: keyof DefaultTheme["colors"]) =>
+	getThemeValue("colors", key);
+
+// theme의 fontSize 부분 가져오는 함수
+export const getFontSize = (key: keyof DefaultTheme["fontSize"]) =>
+	getThemeValue("fontSize", key);
+
+// theme의 fontWeight 부분 가져오는 함수
+export const getFontWeight = (key: keyof DefaultTheme["fontWeight"]) =>
+	getThemeValue("fontWeight", key);
+
+// theme의 breakpoints 부분 가져오는 함수
+export const getBreakPoints = (key: keyof DefaultTheme["breakpoints"]) =>
+	getThemeValue("breakpoints", key);
