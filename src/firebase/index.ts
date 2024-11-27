8f1d58bf5d9635c2a03c2751bf7dbbec3fd19789
박찬희 <fhfh54321@motionimaging.net>
2024-11-27 10:48:11 +0900

    npm run dev 오류 발생하여 해결 및 db초기화 코드 추가

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "@firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,6 +16,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app); // 인증정보(로그인 정보) 가져오기
+export const auth = getAuth(app); // 인증정보(로그인 정보) 가져오기 로그인 시 import하여 활용
+
+export const db = getFirestore(app); //firestore db 초기화 db 사용시 import하여 활용
