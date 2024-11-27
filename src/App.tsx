7cfe31cf95a821782006fdeab0531877cb1da69b
mgyang53 <mg960503@gmail.com>
2024-11-27 15:21:25 +0900

    style: 공통 스타일 import 변수 이름 수정-#7

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import Router from './router';
-import { RouterProvider } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyles } from './styles/globalStyles';
-import { theme } from './styles/theme';
+import Router from "./router";
+import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle, DefaultTheme } from "./styles";
 
 const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <RouterProvider router={Router} />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={DefaultTheme}>
+			<GlobalStyle />
+			<RouterProvider router={Router} />
+		</ThemeProvider>
+	);
 };
 
 export default App;
