b514cdcb30e57bf75ba1e178e5d87fa61829a134
mgYang53 <50770004+mgYang53@users.noreply.github.com>
2024-11-27 16:23:39 +0900

    Feat: global style 및 theme 적용-#7 (#9)
    * feat : 테마 설정 파일 추가-#7
    * feat: 폰트파일 추가 및 global style 적용-#7
    * style: 공통 스타일 import 변수 이름 수정-#7

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,50 @@
+import "styled-components";
+
+declare module "styled-components" {
+	export interface DefaultTheme {
+		colors: {
+			primary: string;
+
+			secondaryDark: string;
+			secondary: string;
+			secondaryLight: string;
+
+			danger: string;
+			warning: string;
+			orange: string;
+			success: string;
+
+			grayDark: string;
+			gray: string;
+			grayLight: string;
+			grayWhite: string;
+
+			white: string;
+
+			dim: string;
+		};
+
+		fontSize: {
+			xs: string;
+			sm: string;
+			md: string;
+			lg: string;
+			xl: string;
+		};
+
+		fontWeight: {
+			light: number;
+			regular: number;
+			medium: number;
+			bold: number;
+		};
+
+		breakpoints: {
+			sm: string;
+			md: string;
+			lg: string;
+			xl: string;
+			xxl: string;
+		};
+	}
+}
