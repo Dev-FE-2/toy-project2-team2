96c4816ada0a5d677f7ad6d1c3b131b0b5991543
박찬희 <fhfh54321@motionimaging.net>
2024-12-02 14:54:51 +0900

    feat: 로그인 기능 개발

--- src/pages/login/Login.styled.ts
+++ src/pages/login/Login.styled.ts
@@ -1,5 +1,10 @@
 import { flexCenter } from "@/styles/commonStyles";
-import { getColor, getFontSize, getFontWeight } from "@/styles/theme";
+import {
+	getBorderRadius,
+	getColor,
+	getFontSize,
+	getFontWeight,
+} from "@/styles/theme";
 import styled from "styled-components";
 
 export const LoginContainer = styled.div`
@@ -7,6 +12,7 @@ export const LoginContainer = styled.div`
 	height: 550px;
 	box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
 	border: 1px solid ${getColor("secondaryLight")};
+	border-radius: ${getBorderRadius("md")};
 	${flexCenter}
 
 	h1 {
@@ -22,12 +28,12 @@ export const LoginBox = styled.div`
 export const LoginHeader = styled.h1`
 	font-size: ${getFontSize("xl")};
 	font-weight: ${getFontWeight("bold")};
-	display: inline;
 `;
-export const LoginForm = styled.div`
+export const LoginForm = styled.form`
 	margin: 0 -10px;
 	${flexCenter}
 	flex-direction: column;
+
 	div {
 		width: 420px;
 	}
@@ -35,10 +41,16 @@ export const LoginForm = styled.div`
 		margin: 10px;
 	}
 `;
-export const HelperText = styled.div`
+export const HelperText = styled.p<{
+	isIdError?: boolean;
+	isPasswordError?: boolean;
+}>`
+	width: 100%;
+	margin: 10px 0;
 	padding: 0 10px;
 	color: ${getColor("danger")};
-	display: ${(props) => (props.isError ? "block" : "none")};
+	display: ${(props) =>
+		props.isIdError ? "block" : props.isPasswordError ? "block " : "none"};
 `;
 export const SignUpGuide = styled.div`
 	margin-top: 30px;
