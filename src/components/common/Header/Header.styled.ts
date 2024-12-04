643f4ba69e3d2894a646b8490d25a465c4c39f0e
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-12-04 13:51:51 +0900

    feat: header z-index 수정 -#21

--- src/components/common/Header/Header.styled.ts
+++ src/components/common/Header/Header.styled.ts
@@ -13,6 +13,7 @@ export const HeaderContainer = styled.header`
 	padding: 15px 40px;
 	border-bottom: 1px solid ${getColor("secondaryDark")};
 	background-color: ${getColor("white")};
+	z-index: 1000;
 `;
 
 export const LeftContainer = styled.div`
