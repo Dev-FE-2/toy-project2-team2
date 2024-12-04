b0c10522bc4ff4d69072c3decf55a670d603a773
mgyang53 <mg960503@gmail.com>
2024-12-05 00:35:55 +0900

    feat: 일정 레이아웃 추가-#27

---
+++ src/layouts/schedule/schedule.styled.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ScheduleWrapper = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	flex-direction: column;
+	min-height: 100vh;
+`;
+
+export const Main = styled.main`
+	padding-top: 102px;
+	width: 100%;
+`;
+
+export const ScheduleContainer = styled.div`
+	width: 100%;
+	max-width: 1500px;
+	padding: 0 30px;
+	margin: 0 auto;
+	overflow: hidden;
+`;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import { MainLayout, AuthLayout } from "@/layouts";
-import HomePage from "@/pages/home";
+import { MainLayout, AuthLayout, ScheduleLayout } from "@/layouts";
 import Schedule from "@/pages/schedule";
 import LoginPage from "@/pages/login";
 import SignupPage from "@/pages/signup";
@@ -8,17 +7,21 @@ import PayPage from "@/pages/pay";
 import PayCorrectionPage from "@/pages/pay-correction";
 import MyPage from "@/pages/mypage";
 import NotFoundPage from "@/pages/notFound";
+import ComponentExample from "@/pages/componentExample";
 
 const Router = createBrowserRouter([
 	{
 		element: <MainLayout />,
 		children: [
-			{ path: "/", element: <HomePage /> },
-			{ path: "/schedule", element: <Schedule /> },
 			{ path: "/pay", element: <PayPage /> },
 			{ path: "/pay-correction", element: <PayCorrectionPage /> },
 			{ path: "/mypage", element: <MyPage /> },
+			{ path: "/components", element: <ComponentExample /> },
 		],
+	},
+	{
+		element: <ScheduleLayout />,
+		children: [{ path: "/", element: <Schedule /> }],
 	},
 	{
 		element: <AuthLayout />,
