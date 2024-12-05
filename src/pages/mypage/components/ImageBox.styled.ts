f3da6e0667e951b9a1a8248fa5b5901a183e103b
박찬희 <fhfh54321@motionimaging.net>
2024-12-05 16:56:15 +0900

    feat: 마이페이지 ImageBox 컴포넌트 추가

---
+++ src/pages/mypage/components/ImageBox.styled.ts
@@ -0,0 +1,20 @@
+import { flexCenter } from "@/styles/commonStyles";
+import styled from "styled-components";
+
+export const ImageBoxContianer = styled.div`
+	${flexCenter}
+	flex-direction: column;
+	button {
+		padding: 13px 0;
+	}
+`;
+export const ImageCircle = styled.div`
+	width: 150px;
+	height: 150px;
+	border-radius: 50%;
+	margin-bottom: 20px;
+`;
+export const UserImage = styled.img`
+	width: 100%;
+	height: 100%;
+`;
