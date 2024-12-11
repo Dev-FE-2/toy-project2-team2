b8ec6e3182ceeecb9c4dee022865e25b80f8d72a
박찬희 <fhfh54321@motionimaging.net>
2024-12-11 14:18:38 +0900

    fix:새로고침 시 isLogined(false)되는 문제 수정 및 uid 관련 문제 수정

--- src/router/ProtectedRoute.tsx
+++ src/router/ProtectedRoute.tsx
@@ -2,26 +2,35 @@ import { Navigate } from "react-router-dom";
 import { auth } from "@/firebase";
 import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { RootState } from "@/types/store";
+import { setIsLogined } from "@/store/slices/loginAuthSlice";
 
 export const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-	const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+	const dispatch = useDispatch();
+	const isLogined = useSelector(
+		(state: RootState) => state.loginAuth.isLogined,
+	);
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		const unsubscribe = onAuthStateChanged(auth, (user) => {
 			if (user) {
-				setIsAuthenticated(true);
+				dispatch(setIsLogined(true));
 			} else {
-				setIsAuthenticated(false);
+				dispatch(setIsLogined(false));
 			}
+			setIsLoading(false);
 		});
-
 		return () => unsubscribe();
-	}, [auth]);
+	}, []);
 
-	if (isAuthenticated === null) {
+	if (isLoading) {
 		return <div>Loading...</div>;
 	}
-	if (!isAuthenticated) {
+
+	if (!isLogined) {
 		return <Navigate to="/login" replace />;
 	}
 	return children;
