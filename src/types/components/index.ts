6a6ebe5a948ec5d5bc016ce19e25979f6301ee9e
mgyang53 <mg960503@gmail.com>
2024-12-12 11:49:04 +0900

    rename: 공통 타입 파일 위치 수정-#39

---
+++ src/types/components/index.ts
@@ -0,0 +1,6 @@
+export type * from "./button";
+export type * from "./input";
+export type * from "./modal";
+export type * from "./select";
+export type * from "./textarea";
+export type * from "./title";
