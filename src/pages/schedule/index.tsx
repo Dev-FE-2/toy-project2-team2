641d91b239092080d2028ce2109180970b3cce92
mgyang53 <mg960503@gmail.com>
2024-12-12 13:42:48 +0900

    feat: 캘린더 일정 상태 context에서 redux로 변경-#39

--- src/pages/schedule/index.tsx
+++ src/pages/schedule/index.tsx
@@ -1,15 +1,28 @@
-import Title from "@/components/Title";
+import { useEffect } from "react";
+import { startOfToday } from "date-fns";
+import { Title } from "@/components";
 import { Calendar } from "./components/Calendar";
 import { Container } from "./schedule.styled";
 import DetailPanel from "./components/Detail/DetailPanel";
+import { fetchSchedules } from "@/store/slices/scheduleSlice";
+import { useAppDispatch } from "@/hooks";
+import { convertTimeZone } from "@/utils";
 
 const Schedule = () => {
+	const today = convertTimeZone(startOfToday());
+	const dispatch = useAppDispatch();
+
+	// 기본적으로 현재 월 데이터를 로드
+	useEffect(() => {
+		dispatch(fetchSchedules({ startDate: today }));
+	}, [dispatch, today]);
+
 	return (
 		<>
 			<Title title={"내 일정"} />
 			<Container>
 				<Calendar />
-				<DetailPanel date={"2024-12-12"} />
+				<DetailPanel />
 			</Container>
 		</>
 	);
