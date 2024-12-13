c6594bad2dec9f69a029136134651a9545278f08
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-12-13 15:46:53 +0900

    fix: 헤더 텍스트 변경-#56

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -34,7 +34,7 @@ const Header = () => {
 
 				<Nav>
 					<NavLink to="/">내 일정</NavLink>
-					<NavLink to="/salary">급여내역 조회</NavLink>
+					<NavLink to="/salary">급여 내역 조회</NavLink>
 					<NavLink to="/salary-correction">정정 신청 내역</NavLink>
 					<NavLink to="/mypage">마이페이지</NavLink>
 				</Nav>
